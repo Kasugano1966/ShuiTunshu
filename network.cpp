6b36c827bb477cc8198db7d8de7c0af43a332f09
Kasugano1966 <haruka5033@proton.me>
2024-08-13 17:53:06 +0800

    Update network.cpp

--- network.cpp
+++ network.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    cout << "水豚鼠大号1134829302” << endl;
+    cout << "水豚鼠大号1134829302" << endl;
     cout << "水豚鼠小号3657191059" << endl;
     cout << "水豚鼠小号3927593450" << endl;
     cout << "水豚鼠小号957949847" << endl;
