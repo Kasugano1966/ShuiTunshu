e8572127a802a740dc18459214b861a2202eb9df
Kasugano1966 <haruka5033@proton.me>
2024-08-13 12:54:18 +0800

    Add files via upload

---
+++ network.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "水豚鼠大号1134829302” << endl;
+    cout << "水豚鼠小号3657191059" << endl;
+    cout << "水豚鼠小号3927593450" << endl;
+    return 0;
+}
